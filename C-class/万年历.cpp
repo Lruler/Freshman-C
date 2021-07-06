8fbda072f9283b22b1ab1833b58539f7b585be27
Lruler <1204312199@qq.com>
2021-07-06 08:48:10 +0800

    1

--- C-class/万年历.cpp
+++ C-class/万年历.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int year(int y) //判断是否是闰年
 {
     if ((y % 4 == 0) && (y % 100 != 0) || y % 400 == 0)
@@ -7,25 +6,19 @@ int year(int y) //判断是否是闰年
     else
         return 365;
 }
-
 int main() //
 {
     int y;
     int i, j, sum = 0;
     int begin, week;
     int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
     scanf("%d", &y);
     for (i = 1; i < y; i++)
         sum += year(i);
-
     week = (sum + 1) % 7; //表示该年1月1日为星期几
-
     if (year(y) == 366)
         days[1] = 29;
-
     printf("\n%d年日历如下：\n\n", y);
-
     for (i = 0; i < 12; i++)
     {
         printf("       %d月          \n", i + 1);
@@ -44,6 +37,5 @@ int main() //
         }
         printf("\n\n");
     }
-
     return 0;
 }
